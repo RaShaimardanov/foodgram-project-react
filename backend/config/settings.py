23f6318d15abf94d088e8e30debb1c2a5eba3a86
Расим Шаймарданов <shaimardanoff92@yandex.ru>
2023-08-12 16:47:44 +0300

    v1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = bool(os.getenv('DEBUG', 'False').lower() == 'true')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split()
+ALLOWED_HOSTS = ['food.cl02.store']
 
 
 # Application definition
