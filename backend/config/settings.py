6eab93ceaf8f6fd5a8d455f1d8fe25e3cad7b4a4
Расим Шаймарданов <shaimardanoff92@yandex.ru>
2023-08-23 09:45:54 +0300

    v1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,14 +79,6 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
-
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
-    ],
-    'SEARCH_PARAM': 'name',
-
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
